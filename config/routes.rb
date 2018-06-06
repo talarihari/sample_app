78d13fa9e8923aea981dc5d4cebadac6c0dca6c3
hari <you@example.com>
2018-06-06 11:55:25 +0530

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :members
+  devise_scope :member do
+  get '/members/sign_out' => 'devise/sessions#destroy'
+  end
   get 'sessions/create'
   get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

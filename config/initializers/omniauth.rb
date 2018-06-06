26fcecc8cd064372ffeb94fdcc140d8548b609b8
hari <you@example.com>
2018-06-06 11:21:32 +0530

    total commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "1070530698701-nb5mc4id09j7s6jrutd4endenclrajov.apps.googleusercontent.com", "D151v0KK_z2K0j8Hx5263n3b"
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'home', to: 'home#show'
+  get 'me', to: 'me#show', as: 'me'
+
+  root to: "home#show"
+
+end
